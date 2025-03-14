81681b72a3d69f6a8a34e45e167e2bf875d9eca4
Pedro Sanches <pedromgsanches@gmail.com>
2025-03-14 10:41:42 +0000

    Delete elasticsearch directory

--- elasticsearch/DownloadBIN.sh
+++
@@ -1,7 +0,0 @@
-export VERSION=$(grep es_version inventory.ini | sed 's/es_version=\|"//g')
-export ARCH=$(grep es_arch inventory.ini | sed 's/es_arch=\|"//g')
-
-cd files
-
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${VERSION}-linux-{$ARCH}.tar.gz
-
